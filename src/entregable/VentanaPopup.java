2333e5cc6517a1c05dfe547e0608e23e657845ce
lean04e <lean.mz95@gmail.com>
2023-12-02 17:07:56 -0300

    Añadi la Ventana Popup...

---
+++ src/entregable/VentanaPopup.java
@@ -0,0 +1,47 @@
+package entregable;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class VentanaPopup {
+	private JFrame frame;
+	private Dimension dmVentana = new Dimension(600, 200);
+	private JButton boton = new JButton("OK");
+	
+	public VentanaPopup(String sss) {
+		Dimension dmPantalla = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (dmPantalla.width - dmVentana.width) / 2;
+		int y = (dmPantalla.height - dmVentana.height) / 2;
+		
+		frame = new JFrame("Resultado");
+		frame.setSize(dmVentana);
+		frame.setLocation(x, y);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		frame.setContentPane(panel);
+		panel.setLayout(null);
+		
+		JLabel lblLabel = new JLabel(sss);
+		lblLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+		lblLabel.setBounds(42, 30, 555, 49);
+		panel.add(lblLabel);
+		
+		boton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		boton.setBounds(240, 110, 105, 27);
+		panel.add(boton);
+		
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
+}
--- src/game/components/SegundaEvaluacionUI.java
+++ src/game/components/SegundaEvaluacionUI.java
@@ -2,6 +2,8 @@ package game.components;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
