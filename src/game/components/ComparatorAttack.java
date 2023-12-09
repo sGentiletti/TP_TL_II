2ec158be3ef4d856c6b4d4d62baa4ae3f83a572a
lbudnik <lucasbudnik@hotmail.com.ar>
2023-12-08 21:11:19 -0300

    se agregaron las funciones compare a ComparatorLife y ComparatorAttack

--- src/game/components/ComparatorAttack.java
+++ src/game/components/ComparatorAttack.java
@@ -1,5 +1,7 @@
 package game.components;
 
-public class ComparatorAttack {
-
+public class ComparatorAttack implements Comparable<Monster>{
+	public int compare(Monster monster1, Monster monster2) {
+		return monster1.activeSkill.damage(monster2) - monster2.activeSkill.damage(monster1);
+	}
 }
--- src/game/components/ComparatorLife.java
+++ src/game/components/ComparatorLife.java
@@ -1,6 +1,6 @@
 package game.components;
 import java.util.Comparator;
-public class ComparatorLife implements Comparator<Monster>{
+public class ComparatorLife implements Comparable<Monster>{
 	public int compare(Monster monster1, Monster monster2) {
 		return monster1.getLife() - monster2.getLife();
 	}
